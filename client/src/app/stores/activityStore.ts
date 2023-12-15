74de03c9ef3612be86af3b1b0750b3e60127a04a
ClindellTassin <clindelljtassin@gmail.com>
2023-12-14 22:12:32 -0600

    added signalR for comments

--- client/src/app/stores/activityStore.ts
+++ client/src/app/stores/activityStore.ts
@@ -168,4 +168,8 @@ export default class ActivityStore {
             runInAction(() => this.loading = false);
         }
     };
+
+    clearSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    };
 }
--- client/src/features/activities/details/ActivityDetails.tsx
+++ client/src/features/activities/details/ActivityDetails.tsx
@@ -11,12 +11,13 @@ import ActivityDetailsSidebar from "./ActivityDetailsSidebar";
 
 const ActivityDetails = () => {
     const { activityStore } = useStore();
-    const { selectedActivity: activity, loadActivity, loadingInitial } = activityStore;
+    const { selectedActivity: activity, loadActivity, loadingInitial, clearSelectedActivity } = activityStore;
     const { id } = useParams();
 
     useEffect(() => {
         if (id) loadActivity(id);
-    }, [id, loadActivity]);
+        return () => clearSelectedActivity();
+    }, [id, loadActivity, clearSelectedActivity]);
 
     if (loadingInitial || !activity) return <LoadingComponent />;
 
@@ -25,7 +26,7 @@ const ActivityDetails = () => {
             <Grid.Column width={10}>
                 <ActivityDetailsHeader activity={activity} />
                 <ActivityDetailsInfo activity={activity} />
-                <ActivityDetailsChat />
+                <ActivityDetailsChat activityId={activity.id} />
             </Grid.Column>
             <Grid.Column width={6}>
                 <ActivityDetailsSidebar activity={activity} />
